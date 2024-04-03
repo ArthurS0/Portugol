3cc08e6899f95d8f29e9dd32a425fb204d0eb489
ArthurS0 <160639820+ArthurS0@users.noreply.github.com>
2024-04-03 10:45:28 -0300

    idade da Luíza

--- main (1).cpp
+++ main (1).cpp
@@ -1,23 +1,25 @@
 #include <stdio.h>
 #include <locale.h>
-int main(){
+int main (){
     
-    char nome[50];
-    float salario, percentual, novo_salario;
-    
-    printf("Digite o nome da pessoa: ");
-    scanf("%s", nome);
-    
-    printf("Digite o salario da pessoa: R$");
-    scanf("%f", &salario);
-    
-    printf("Digite o valor do percentual: ");
-    scanf("%f", &percentual);
-    
-    novo_salario = salario *(1+percentual/100);
-    
-    printf("Novo salario de %s: R$%.2f\n", nome, novo_salario);
-    
-    return 0;
-    
-}    
+char nome[50];    
+float idade;
+
+printf("Digite o nome da pessoa: ");
+scanf("%s", nome);
+
+printf("Digite a idade da pessoa: ");
+scanf("%f", &idade);
+
+printf("A idade de %s é: %1.f\n", nome, idade);
+if(idade>=18){
+    printf("Adulto");
+}else{
+    printf("Criança");
+}
+
+return 0;
+
+}
+
+
